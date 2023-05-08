0ddcaf372fc35b3187bd50eff569767407747dac
Jefferson Alves <j.alves@dxc.com>
2023-05-08 19:48:28 -0300

    Insersão de clientes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,30 @@
 package org.example;
+import org.example.domain.repositorio.Clientes;
+import org.example.domain.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Main {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes) {
+            return args -> {
+                Cliente cliente = new Cliente("Jefferson");
+                clientes.salvar(cliente);
+
+                Cliente cliente2 = new Cliente("Raquel");
+                clientes.salvar(cliente2);
+            };
+
+    }
 
     @Autowired
     @Qualifier("applicationName")
---
+++ src/main/java/org/example/domain/entity/Cliente.java
@@ -0,0 +1,29 @@
+package org.example.domain.entity;
+
+public class Cliente {
+    private Integer id;
+    private String nome;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
